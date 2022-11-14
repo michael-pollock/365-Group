9be48755bc7b2974459d46846a90de21db5a7fe5
Ryan Dickinson <rydickin@nmu.edu>
2022-11-14 16:03:05 -0500

    Set up simple express server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,17 @@
-/*
-    Blank server javascript file
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const PORT = process.env.PORT || 4876;
 
-*/
+app.use(express.static("client"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
