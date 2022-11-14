88ba408b9cb17ba44694abf3d65e6488b326653a
Ryan Dickinson <rydickin@nmu.edu>
2022-11-14 16:09:08 -0500

    Added environment variables for the port checkout .env file

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 4876;
+require("dotenv").config();
 
 app.use(express.static("client"));
 
