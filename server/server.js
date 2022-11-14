2825c56dbf771d8d1efcfeef488672e83c736df2
Ryan Dickinson <rydickin@nmu.edu>
2022-11-14 18:48:49 -0500

    Changed app.get() in server.js file

--- server/server.js
+++ server/server.js
@@ -5,12 +5,11 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 4876;
-require("dotenv").config();
 
 app.use(express.static("client"));
 
-app.get("/", (request, response) => {
-  res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(PORT, () => {
