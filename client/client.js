8db0f05edac8c8ef8a7c34c7291940b9fa197a66
Ryan Dickinson <rydickin@nmu.edu>
2022-11-16 18:24:31 -0500

    Made changes to board

--- client/client.js
+++ client/client.js
@@ -4,6 +4,32 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
+      size: 6,
+      board: [[], []],
+      boardHeader: "A B C D E F G",
     };
+  },
+  methods: {
+    fillBoard() {
+      let xIndex = 0;
+      let yIndex = 0;
+      let i = 0;
+      let j = 0;
+      for (i; i <= this.size; i++) {
+        for (j; j <= this.size; j++) {
+          if (i == 0 && j > 0) {
+            this.board[i][j] = xIndex;
+          }
+          if (i !== 0 && j == 0) {
+            this.board[i][j] = yIndex;
+          }
+          xIndex++;
+        }
+        yIndex++;
+      }
+    },
+  },
+  mounted() {
+    this.fillBoard();
   },
 }).mount("#app");
