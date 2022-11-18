fd3becd77a81a6bcb602af1cdea7168013c4d0c4
Ryan Dickinson <rydickin@nmu.edu>
2022-11-17 19:42:24 -0500

    Added Chat Box functionality with Socket.io to allow multiple users to connect with a unique id number from 0-100 and it displays which user is logged in currently in browser window

--- server/server.js
+++ server/server.js
@@ -14,6 +14,28 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+let users = {};
+
+function addUserList() {
+  let ret = [];
+  for (i in users) ret.push(users[i]);
+  return ret;
+}
+
+function randomUserId() {
+  return Math.floor(Math.random() * 100);
+}
+
+io.on("connection", function (socket) {
+  users[socket.id] = {
+    userId: randomUserId(),
+  };
+
+  console.log(users[socket.id] + " connected with id " + socket.id);
+  socket.emit("sendUserId", users[socket.id].userId);
+  io.emit("sendUsersList", addUserList()); //io.emit() broadcasts to all sockets that are connected!
+});
+
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
