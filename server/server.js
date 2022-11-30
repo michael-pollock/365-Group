c7252736730a2ab382da7d88ef756ae652aa4cf4
Ryan Dickinson <rydickin@nmu.edu>
2022-11-30 15:12:05 -0500

    Overhauled server structure removed usernames userids add playernumbers

--- server/server.js
+++ server/server.js
@@ -12,45 +12,42 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-let numOfClients = 0;
 let users = {};
 let player1Ships = [];
 let player1Board = [];
 let player2Ships = [];
 let player2Board = [];
 
+const playerConnections = [null, null];
+
 io.on("connection", function (socket) {
+  let playerIndex = -1;
+  for (const i in playerConnections) {
+    if (playerConnections[i] === null) {
+      playerIndex = i;
+      break;
+    }
+  }
+
+  socket.emit("player-num", playerIndex);
+  console.log(`Player ${playerIndex} connected`);
+
+  if (playerIndex === -1) {
+    return;
+  }
+
+  playerConnections[playerIndex] = false;
+
+  socket.broadcast.emit("player-connected", playerIndex);
+
   socket.on("disconnect", function () {
-    console.log(users[socket.id].userId + " disconnected.");
-    delete users[socket.id];
-    io.emit("sendUsersList", updateUserList());
+    console.log(`Player ${playerIndex} disconnected`);
+    playerConnections[playerIndex] = null;
+
+    socket.broadcast.emit("player-connection", playerIndex);
   });
-
-  users[socket.id] = {
-    userId: randomUserId(),
-  };
-
-  console.log(users[socket.id] + " connected with id " + socket.id);
-  socket.emit("sendUserId", users[socket.id].userId);
-  io.emit("sendUsersList", updateUserList()); //io.emit() broadcasts to all sockets that are connected!
 });
 
 server.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-
-function updateUserList() {
-  let ret = [];
-  for (i in users) ret.push(users[i]);
-  return ret;
-}
-
-function randomUserId() {
-  return Math.floor(Math.random() * 100);
-}
-
-function addUserList() {
-  let ret = [];
-  for (i in users) ret.push(users[i]);
-  return ret;
-}
