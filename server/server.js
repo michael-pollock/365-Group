4a6ab072eedbf08f738aa5c481b9ae76c7c2105e
Ryan Dickinson <rydickin@nmu.edu>
2022-12-02 09:08:58 -0500

    Changed server.js

--- server/server.js
+++ server/server.js
@@ -13,10 +13,6 @@ app.get("/", (req, res) => {
 });
 
 let users = {};
-let player1Ships = [];
-let player1Board = [];
-let player2Ships = [];
-let player2Board = [];
 
 const playerConnections = [null, null];
 
